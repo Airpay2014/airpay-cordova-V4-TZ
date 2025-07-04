f0e2c82c2122719903054dae65d23391784929aa
abhijeet.mule <abhijeet.mule@airpay.co.in>
2025-07-04 20:18:15 +0530

    Initial commit for cordova plugin

--- src/android/MyPlugin.java
+++ src/android/MyPlugin.java
@@ -5,20 +5,16 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
-
-
-import com.airpay.airpaysdk_simplifiedotp.AirpayConfig;
-import com.airpay.airpaysdk_simplifiedotp.constants.ConfigConstants;
-import com.airpay.airpaysdk_simplifiedotp.utils.Utils;
-import com.example.cordovaapp.MainActivity;
-
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaWebView;
+import com.airpay.ae.AirpayConfig;
+import com.airpay.ae.constants.ConfigConstants;
+import com.airpay.ae.utils.Utility;
+import com.airpay.samplecordova.MainActivity;
 import org.json.JSONObject;
-import android.text.TextUtils;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
 
 public class MyPlugin extends CordovaPlugin  {
     private static final String TAG = "MyPlugin";
@@ -28,18 +24,11 @@ public class MyPlugin extends CordovaPlugin  {
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-
-
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        // if (action.equals("greet")) {
-        //     String name = args.getString(0);
-        //     this.greet(name, callbackContext);
-        //     return true;
-        // }
-        // return false;
+
 
         if (action.equals("greet")) {
             this.callbackContext = callbackContext;  // Save callback context to return result later
@@ -74,66 +63,38 @@ public class MyPlugin extends CordovaPlugin  {
             String city = formData.getString("city");
             String state = formData.getString("state");
             String country = formData.getString("country");
-            String pincode = formData.getString("pincode");
             String orderId = formData.getString("orderId");
             String amount = formData.getString("amount");
-            String txnSubtype = formData.getString("txnSubtype");
-            String nextRunDate = formData.getString("nextRunDate");
-            String period = formData.getString("period");
-            String frequency = formData.getString("frequency");
-            String maxAmount = formData.getString("maxAmount");
-            String subscriptionAmount = formData.getString("subscriptionAmount");
-            String recurringCount = formData.getString("recurringCount");
-            String retryAttempts = formData.getString("retryAttempts");
 
-
-            String sAllData1;
             DateFormat df1 = new SimpleDateFormat("yyyy-MM-dd");
             String sCurDate1 = df1.format(new Date());
 
 
 
-            if (!TextUtils.isEmpty(txnSubtype) && txnSubtype.equals("12")) {
-
-                if ("A".equalsIgnoreCase(period)) {
-                    sAllSubsciptionData = period +
-                            appendDecimal(subscriptionAmount) +
-                            "1" +
-                            retryAttempts;
-                } else {
-                    sAllSubsciptionData = nextRunDate +
-                            frequency +
-                            period +
-                            appendDecimal(subscriptionAmount) +
-                            "1" +
-                            recurringCount +
-                            retryAttempts;
-                }
-
-
-                sAllData1 = email + firstName
-                        + lastName + address
-                        + city + state
-                        + country + appendDecimal(amount)
-                        + orderId + sAllSubsciptionData + sCurDate1;
-
-
-            } else {
-                sAllData1 = email + firstName
-                        + lastName + address
-                        + city + state
-                        + country + appendDecimal(amount)
-                        + orderId + sCurDate1;
-            }
+            String sAllData = email + firstName
+                    + lastName + address
+                    + city + state
+                    + country + amount
+                    + orderId + sCurDate1;
 
             // Merchant details
-            Log.d("sAllData1", "" + sAllData1);
+            Log.d("sAllData1", "" + sAllData);
 
-            // Merchant details
             String sMid = "";
             String sSecret = "";
             String sUserName = "";
             String sPassword = "";
+            // private key
+            String sTemp = sSecret + "@" + sUserName + ":|:" + sPassword;
+            String sPrivateKey = Utility.sha256(sTemp);
+
+            // key for checksum
+            String sTempUserPasswd = sUserName + "~:~" + sPassword;
+            String sKey = Utility.sha256(sTempUserPasswd);
+
+            // checksum
+            sAllData = sKey + "@" + sAllData;
+            String sChecksum = Utility.sha256(sAllData);
 
             String merdom = "";
             String successUrl = "";
@@ -141,66 +102,42 @@ public class MyPlugin extends CordovaPlugin  {
 
             String client_id = "";
             String client_secret = "";
-            // private key
-            String sTemp = sSecret + "@" + sUserName + ":|:" + sPassword;
-            String sPrivateKey = Utils.sha256(sTemp);
-
-            // key for checksum
-            String sTemp3 = sUserName + "~:~" + sPassword;
-            String sKey1 = Utils.sha256(sTemp3);
-
-            // checksum
-            sAllData1 = sKey1 + "@" + sAllData1;
 
 
-            String sChecksum1 = Utils.sha256(sAllData1);
-            Log.d("Checksum", sChecksum1);
-
-            AirpayConfig.Builder builder = new AirpayConfig.Builder(cordova.getActivity(), ((MainActivity) cordova.getActivity()).getActivityResultLauncher());
-            builder.setEnvironment(ConfigConstants.PRODUCTION);
-            builder.setType(ConfigConstants.AIRPAY_KIT);
-            builder.setPrivateKey(sPrivateKey);
-            builder.setMerchantId(sMid);
-            builder.setOrderId(orderId);
-            builder.setCurrency("356");
-            builder.setIsoCurrency("INR");
-            builder.setEmailId(email);
-            builder.setMobileNo(phone);
-            builder.setBuyerFirstName(firstName);
-            builder.setBuyerLastName(lastName);
-            builder.setBuyerAddress(address);
-            builder.setBuyerCity(city);
-            builder.setBuyerState(state);
-            builder.setBuyerCountry(country);
-            builder.setBuyerPinCode(pincode);
-            builder.setAmount(appendDecimal(amount));
-            builder.setWallet("0");
-            builder.setCustomVar("");
-            builder.setTxnSubType(txnSubtype);
-            builder.setChmod("");
-            builder.setChecksum(sChecksum1);
-            builder.setMerDom(merdom);
-            builder.setSuccessUrl(successUrl);
-            builder.setFailedUrl(failureUrl);
-            builder.setLanguage("EN");
-            builder.setClient_id(client_id);
-            builder.setClient_secret(client_secret);
-            builder.setGrant_type("client_credentials");
-            builder.setAesDesKey(sTemp3);
-            if (!TextUtils.isEmpty(txnSubtype) && txnSubtype.equals("12")) {
-                //new AirpayConfig.Builder(MainActivity.this, activityResultLauncher)
-                builder.setNextrundate(nextRunDate);
-                builder.setPeriod(period);
-                builder.setFrequency(frequency);
-                builder.setMaxAmount(appendDecimal(maxAmount));
-                builder.setSubscriptionAmt(appendDecimal(subscriptionAmount));
-                builder.setIsRecurring("1");
-                builder.setRecurringCount(recurringCount);
-                builder.setRetryAttempts(retryAttempts);
-                builder.build().initiatePayment();
-            } else {
-                builder.build().initiatePayment();
-            }
+            new AirpayConfig.Builder(((MainActivity) cordova.getActivity()).getActivityResultLauncher(),cordova.getActivity())
+                    .setActionType("IndexPay")
+                    .setEnvironment(ConfigConstants.PRODUCTION)
+                    .setType(ConfigConstants.AIRPAY_KIT)
+                    .setPrivateKey(sPrivateKey)
+                    .setSecretKey(sSecret)
+                    .setMerchantId(sMid)
+                    .setOrderId(orderId)
+                    .setCurrency("834") // 834    840
+                    .setIsoCurrency("tzs") // tzs  usd
+                    .setEmailId(email)
+                    .setMobileNo(phone)
+                    .setBuyerFirstName(firstName)
+                    .setBuyerLastName(lastName)
+                    .setBuyerAddress(address)
+                    .setBuyerCity(city)
+                    .setBuyerState(state)
+                    .setBuyerCountry(country)
+                    .setAmount(amount)
+                    .setWallet("0")
+                    .setChmod("")
+                    .setChecksum(sChecksum)
+                    .setMerDom(merdom)
+                    .setSuccessUrl(successUrl)
+                    .setFailedUrl(failureUrl)
+                    .setTxnsubtype("")
+                    .setCustomVar("")
+                    .setLanguage("EN")
+                    .setClient_id(client_id)
+                    .setClient_secret(client_secret)
+                    .setGrant_type("client_credentials")
+                    .setAesDesKey(sTempUserPasswd)
+                    .build()
+                    .initiatePayment();
 
 
             callbackContext.success("Payment initiated with form data");
@@ -209,17 +146,6 @@ public class MyPlugin extends CordovaPlugin  {
         }
     }
 
-    public String appendDecimal(String input) {
-        if (input == null || input.isEmpty()) {
-            return "0.00"; // Default value for empty input
-        }
 
-        // Check if input already has a decimal
-        if (!input.contains(".")) {
-            return input + ".00"; // Append .00 if no decimal exists
-        }
-
-        return input; // Return as-is if it already has a decimal
-    }
 
 }
